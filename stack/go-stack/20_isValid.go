25cf3a2d2fc038e43d43377f6921a9042d055ced
pickleprat <prat16here@gmail.com>
2025-04-03 15:35:44 +0530

    added isvalid

--- stack/go-stack/20_isValid.go
+++ stack/go-stack/20_isValid.go
@@ -1,1 +1,27 @@
-package main 
+package main
+
+func isValid(s string) bool {
+	openToClose := map[byte]byte{
+		'{': '}',
+		'[': ']',
+		'(': ')',
+	}
+
+	stack := make([]byte, 0)
+
+	for _, runeBracket := range s {
+		bracket := byte(runeBracket)
+		if bracket == '{' || bracket == '(' || bracket == '[' {
+			stack = append(stack, bracket)
+		} else if bracket == '}' || bracket == ')' || bracket == ']' {
+			if openToClose[stack[len(stack)-1]] == bracket {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		}
+
+	}
+
+	return len(stack) == 0
+}
--- stack/go-stack/main.go
+++ stack/go-stack/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 
 func main() {
-	return
+	s1 := "{[{()}]}"; 
+	s2 := "{{[{()}]}"; 
+
+	if isValid(s1) && !isValid(s2)  {
+		fmt.Printf("The strings %s and %s  were correctly evaluated.\n", s1, s2); 
+	} 
 }
