d3c10852db963b8bd50863a278cfad5809144e4a
pickleprat <prat16here@gmail.com>
2025-04-04 17:39:23 +0530

    Solved another one

---
+++ stack/go-stack/reverse-polish.go
@@ -0,0 +1,44 @@
+package main 
+
+// a way to execute an operation between two numbers 
+func Execute(operand string, left int, right int) int {
+    switch operand {
+        case "+": 
+            return left + right; 
+        case "-": 
+            return left - right; 
+        case "/": 
+            return left / right; 
+        case "*": 
+            return left * right; 
+        default: 
+            return 0; 
+    }
+}
+
+func IsOperand(operand string ) bool {
+    return operand == "+" || operand == "-" || operand == "*" || operand == "/"; 
+}
+
+func evalRPN(tokens []string) int {
+    // a way to convert strings to integers between -200 and 200 
+    convertToNumber := make(map[string] int, 0);
+    stack := make([] int, 0);  
+    for i := -200; i <= 200; i++ {
+        convertToNumber[fmt.Sprintf("%d", i)] = i; 
+    }
+
+    for _, token := range tokens {
+        if IsOperand(token) && len(stack) >= 2 {
+            right := stack[len(stack) - 1]; 
+            left := stack[len(stack) - 2]; 
+            executedOutput := Execute(token, left, right);
+            stack = stack[:len(stack) - 1]
+            stack[len(stack) - 1] = executedOutput;  
+        } else {
+            stack = append(stack, convertToNumber[token]); 
+        }
+    }
+
+    return stack[0]; 
+}
